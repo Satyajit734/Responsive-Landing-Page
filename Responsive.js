65e91786d39421ca337946e7f0292abdb46b5505
Satyajit Samanta <contact.satyajitx@gmail.com>
2026-03-09 10:28:32 +0530

    Add files via upload

---
+++ Responsive.js
@@ -0,0 +1,8 @@
+function toggleMenu(){
+  var menu=document.getElementById("mobileMenu");
+  menu.style.display=menu.style.display==="block"?"none":"block";
+}
+
+function closeMenu(){
+  document.getElementById("mobileMenu").style.display="none";
+}
